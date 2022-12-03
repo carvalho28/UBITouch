68d588eee3356934aeda8c2dabb8ef3be0c628f2
carvalho28 <carvalhoo287@gmail.com>
2022-12-03 20:59:26 +0000

    Added base controller for the register with api access
    Signed-off-by: carvalho28 <carvalhoo287@gmail.com>

--- app/src/main/java/pt/ubi/di/pdm/ubitouch/MainActivity.java
+++ app/src/main/java/pt/ubi/di/pdm/ubitouch/MainActivity.java
@@ -2,13 +2,26 @@ package pt.ubi.di.pdm.ubitouch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
+
+        btnMain = findViewById(R.id.btnMain);
+
+        // on click go to register activity
+        btnMain.setOnClickListener(
+                v -> {
+                    Intent intent = new Intent(this, RegisterActivity.class);
+                    startActivity(intent);
+                });
     }
 }
--- app/src/main/java/pt/ubi/di/pdm/ubitouch/RegisterActivity.java
+++ app/src/main/java/pt/ubi/di/pdm/ubitouch/RegisterActivity.java
@@ -1,13 +1,129 @@
 package pt.ubi.di.pdm.ubitouch;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    MaterialButton btnRegister;
+    TextInputEditText firstName, lastName, email, username, password, confirmPassword;
+    TextView msgError;
+
+    // DEBUG
+    private final String TAG = "Diogo";
+
+    // URL of the API
+    private final String URL = "https://server-ubi-touch.herokuapp.com/users/register";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        btnRegister = findViewById(R.id.buttonRegister);
+
+        firstName = findViewById(R.id.inputFirstName);
+        lastName = findViewById(R.id.inputLastName);
+        email = findViewById(R.id.inputEmail);
+        username = findViewById(R.id.inputUsername);
+        password = findViewById(R.id.inputPassword);
+        confirmPassword = findViewById(R.id.inputConfirmPassword);
+        msgError = findViewById(R.id.textViewError);
+
+        Log.i(TAG, "RegisterActivity: onCreate()");
+
+        btnRegister.setOnClickListener(
+                v -> {
+                    // if there is an empty field
+                    if (firstName.getText().toString().isEmpty() || lastName.getText().toString().isEmpty()
+                            || email.getText().toString().isEmpty() || username.getText().toString().isEmpty()
+                            || password.getText().toString().isEmpty()
+                            || confirmPassword.getText().toString().isEmpty()) {
+                        Log.i(TAG, "RegisterActivity: onCreate(): empty field");
+                        msgError.setText("Please fill all the fields");
+                        msgError.setVisibility(TextView.VISIBLE);
+                    }
+                    // if the password and the confirm password are different
+                    else if (!password.getText().toString().equals(confirmPassword.getText().toString())) {
+                        msgError.setText("The password and the confirm password are different");
+                    }
+                    // if the password is less than 10 characters
+                    else if (password.getText().toString().length() < 8) {
+                        msgError.setText("The password must have at least 10 characters");
+                    }
+                    // if the email is not valid
+                    else if (!email.getText().toString().contains("@") || !email.getText().toString().contains(".")) {
+                        msgError.setText("The email is not valid");
+                    }
+                    // create the user
+                    else {
+                        try {
+                            RequestQueue requestQueue = Volley.newRequestQueue(this);
+                            JSONObject jsonObject = new JSONObject();
+                            jsonObject.put("firstName", firstName.getText().toString());
+                            jsonObject.put("lastName", lastName.getText().toString());
+                            jsonObject.put("username", username.getText().toString());
+                            jsonObject.put("email", email.getText().toString());
+                            jsonObject.put("password", password.getText().toString());
+
+                            // make a json object request
+                            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, URL,
+                                    jsonObject,
+                                    response -> {
+                                        Log.i(TAG, "RegisterActivity: onCreate(): response: " + response.toString());
+                                        // go to the login activity
+                                        Intent intent = new Intent(this, LoginActivity.class);
+                                        startActivity(intent);
+                                    },
+                                    error -> {
+                                        Log.i(TAG, "RegisterActivity: onCreate(): error: " + error.toString());
+                                        msgError.setText("The username or the email already exists");
+                                        msgError.setVisibility(TextView.VISIBLE);
+                                        // close the keyboard
+                                        View view = this.getCurrentFocus();
+                                        if (view != null) {
+                                            InputMethodManager imm = (InputMethodManager) getSystemService(
+                                                    Context.INPUT_METHOD_SERVICE);
+                                            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                                        }
+                                        username.requestFocus();
+                                    }) {
+                                @Override
+                                public String getBodyContentType() {
+                                    return "application/json; charset=utf-8";
+                                }
+
+                                @Override
+                                protected Response<JSONObject> parseNetworkResponse(NetworkResponse response) {
+                                    Log.i(TAG, "RegisterActivity: onCreate(): parseNetworkResponse(): "
+                                            + response.toString());
+                                    return super.parseNetworkResponse(response);
+                                }
+                            };
+                            requestQueue.add(jsonObjectRequest);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
     }
 }
