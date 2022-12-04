1e288437705ce7aafd294a4c7070b981ab3cbd45
eduxrdx <edugit88@gmail.com>
2022-12-04 15:47:27 +0000

    Added Feed view and controller
    Also updated string files with content related to the Feed view.

---
+++ app/src/main/java/pt/ubi/di/pdm/ubitouch/FeedActivity.java
@@ -0,0 +1,13 @@
+package pt.ubi.di.pdm.ubitouch;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+    }
+}
