6632a20199bfa88cb9303e3b38c1449f332f15b5
eduxrdx <edugit88@gmail.com>
2022-12-07 17:44:46 +0000

    Added post activity

---
+++ app/src/main/java/pt/ubi/di/pdm/ubitouch/CreateActivity.java
@@ -0,0 +1,14 @@
+package pt.ubi.di.pdm.ubitouch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create);
+    }
+}
