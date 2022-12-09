59eda83b7b6480bf738a2560facb4c9fb96b1206
khajiits <matilde.a.rosa@gmail.com>
2022-12-09 17:27:37 +0000

    Update SettingsActivity.java

--- app/src/main/java/pt/ubi/di/pdm/ubitouch/SettingsActivity.java
+++ app/src/main/java/pt/ubi/di/pdm/ubitouch/SettingsActivity.java
@@ -1,7 +1,7 @@
 package pt.ubi.di.pdm.ubitouch;
-
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
 
