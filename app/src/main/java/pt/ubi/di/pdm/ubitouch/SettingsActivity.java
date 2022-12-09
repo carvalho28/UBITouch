2d4b53e4daacdd4e0820b34fdd867f4378b65936
khajiits <matilde.a.rosa@gmail.com>
2022-12-09 17:25:13 +0000

    Settings view added.

---
+++ app/src/main/java/pt/ubi/di/pdm/ubitouch/SettingsActivity.java
@@ -0,0 +1,13 @@
+package pt.ubi.di.pdm.ubitouch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
