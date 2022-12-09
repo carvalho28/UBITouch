682f7fa9953ea5c985606e010627f15c2f505398
eduxrdx <edugit88@gmail.com>
2022-12-09 00:19:28 +0000

    Date Picker added

--- app/src/main/java/pt/ubi/di/pdm/ubitouch/CreateActivity.java
+++ app/src/main/java/pt/ubi/di/pdm/ubitouch/CreateActivity.java
@@ -2,9 +2,13 @@ package pt.ubi.di.pdm.ubitouch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,12 +18,16 @@ import com.google.android.material.textfield.TextInputEditText;
 
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class CreateActivity extends AppCompatActivity {
 
     ImageView profilePicture;
     Button btnCreatePost, btnAttachFile, btnPickDate, btnPickTime ;
     TextInputEditText createTitle, createDescription;
-    TextView msgError;
+    TextView dateText, timeText, msgError;
+    DatePickerDialog datePickerDialog;
 
     // DEBUG
     private final String TAG = "USER";
@@ -33,9 +41,13 @@ public class CreateActivity extends AppCompatActivity {
 
         createTitle = findViewById(R.id.createTitle);
         createDescription = findViewById(R.id.createDescription);
+        dateText = findViewById(R.id.dateText);
+        timeText = findViewById(R.id.timeText);
         msgError = findViewById(R.id.msgError);
 
+        initDatePicker();
 
+        /*
         btnCreatePost.setOnClickListener(
                 (v) -> {
                     if (createTitle.getText().toString().isEmpty()) {
@@ -61,5 +73,82 @@ public class CreateActivity extends AppCompatActivity {
                     }
                 }
         );
+        */
     }
+
+    // DATE PICKER ---------------------------------------------------------------------------------
+    private void initDatePicker() {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int y, int m, int d) {
+                m += 1;
+                String date = getDateString(d, m, y);
+                dateText.setText(date);
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        int style = AlertDialog.THEME_DEVICE_DEFAULT_DARK;
+
+        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+        datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis() - 1000); // TODAY
+    }
+
+    private String getTodaysDate() {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        month += 1; // because months start at 0
+
+        return getDateString(day, month, year);
+    }
+
+    private String getDateString(int day, int month, int year) {
+        return day + " " + getMonthFormat(month) + " " + year;
+    }
+
+    private String getMonthFormat(int month) {
+        if (month == 1)
+            return getString(R.string.january);
+        if (month == 2)
+            return getString(R.string.february);
+        if (month == 3)
+            return getString(R.string.march);
+        if (month == 4)
+            return getString(R.string.april);
+        if (month == 5)
+            return getString(R.string.may);
+        if (month == 6)
+            return getString(R.string.june);
+        if (month == 7)
+            return getString(R.string.july);
+        if (month == 8)
+            return getString(R.string.august);
+        if (month == 9)
+            return getString(R.string.september);
+        if (month == 10)
+            return getString(R.string.october);
+        if (month == 11)
+            return getString(R.string.november);
+        if (month == 12)
+            return getString(R.string.december);
+
+        // Default
+        return getString(R.string.january);
+    }
+
+    public void openDatePicker(View view) {
+        datePickerDialog.show();
+    }
+    // ---------------------------------------------------------------------------------------------
+
+    // TIME PICKER ---------------------------------------------------------------------------------
+
+    // ---------------------------------------------------------------------------------------------
+
+
 }
