44fd4e46438c9c9d1cfc14193fa96a44f3c4c49e
khajiits <matilde.a.rosa@gmail.com>
2022-12-10 00:49:41 +0000

    Several views and controllers added.
    Specifically change password, deactivate, language and security & privacy.
    Improvements needed.

---
+++ app/src/main/java/pt/ubi/di/pdm/ubitouch/ChangePasswordActivity.java
@@ -0,0 +1,14 @@
+package pt.ubi.di.pdm.ubitouch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChangePasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password);
+    }
+}
