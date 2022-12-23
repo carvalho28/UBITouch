af22452af32c9dfc3646d41e8f5d6a096e027509
khajiits <matilde.a.rosa@gmail.com>
2022-12-23 14:45:01 +0000

    Interests Fragment

--- app/src/main/java/pt/ubi/di/pdm/ubitouch/InterestsFragment.java
+++ app/src/main/java/pt/ubi/di/pdm/ubitouch/InterestsFragment.java
@@ -1,12 +1,44 @@
 package pt.ubi.di.pdm.ubitouch;
 
+import static com.android.volley.VolleyLog.TAG;
+
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class InterestsFragment extends Fragment {
+
+    private int event_creator;
+    private int nOfEvents;
+    private ArrayList<Event> listEvents = new ArrayList<>();
+    private RecyclerView recyclerView;
+
+    private RecyclerAdapter customAdapter;
+
+    // URL
+    private String user_events_URL = "https://server-ubi-touch.herokuapp.com/interests/user/";
 
 
     @Override
@@ -19,6 +51,64 @@ public class InterestsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_interests, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        recyclerView = view.findViewById(R.id.userInterests);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        SharedPreferences sharedPref = this.getActivity().getSharedPreferences("user", Context.MODE_PRIVATE);
+        String userId = sharedPref.getString("id", "false");
+        String token = sharedPref.getString("token", "false");
+
+        event_creator = Integer.parseInt(userId);
+        user_events_URL += event_creator;
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, user_events_URL, null,
+                response -> {
+                    try {
+                        JSONArray events = response.getJSONArray("data");
+                        nOfEvents = events.length();
+                        Log.i(TAG, "N of events: " + nOfEvents);
+
+                        for (int i = 0; i < events.length(); i++) {
+                            JSONObject e = (JSONObject) events.get(i);
+
+                            // int id = Integer.parseInt(e.getString("idEvent"));
+                            String title = e.getString("title");
+                            String description = e.getString("description");
+                            String image = e.getString("image");
+                            String isVerified = e.getString("isVerified");
+                            //String userId = e.getString("idUser");
+                            String eventDate = e.getString("eventDate");
+                            String eventHour = e.getString("eventHour");
+                            String creationDate = e.getString("createdAt");
+                            //String updated_dates = e.getString("updatedAt");
+                            listEvents.add(new Event(title, image, description, eventHour, eventDate, "1", "0"));
+                            // if user is admin then verified flag is visible
+                            // ----- if verified == 1 then it is verified, else verified == 0 it is unverified
+                            // if the user is not an admin then the flag is invisible
+                        }
+
+                        customAdapter = new RecyclerAdapter(getContext(), listEvents);
+                        recyclerView.setAdapter(customAdapter);
+                    } catch (JSONException e) {
+                        Log.e(TAG, "json error");
+                    }
+                },
+                error -> {
+                    // if there was an error TODO
+                    // msgError.setText(R.string.error_msg);
+                    // msgError.setVisibility(View.VISIBLE);
+                    Log.e(TAG, "Error");
+                });
+
+        // add the request to the queue
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+        queue.add(request);
     }
 
 }
